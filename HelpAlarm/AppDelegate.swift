2196a5d7ad1161ee7aab22b0e8daf6ae5426421b
bfleury2017 <bfleury@outlook.com>
2017-07-17 22:33:23 -0400

    Initial

--- HelpAlarm/AppDelegate.swift
+++ HelpAlarm/AppDelegate.swift
@@ -1,39 +1,79 @@
 //
 //  AppDelegate.swift
-//  HelpAlarm
+//  Accelerometer
 //
-//  Created by Brian on 7/17/17.
-//  Copyright © 2017 Fleury Mobile. All rights reserved.
+//  Created by Brian on 3/21/17.
+//  Copyright © 2017 NHTI. All rights reserved.
 //
 
 import UIKit
-
+import MediaPlayer
+import AudioToolbox
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        start()
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
+    func start() {
+        let volumeView = MPVolumeView()
+        if let view = volumeView.subviews.first as? UISlider {
+            //set system volume to max level
+            view.value = 1.0
+        }
+        
+        let filename = "raid"
+        let ext = "m4a"
+        
+        if let soundUrl = Bundle.main.url(forResource: filename, withExtension: ext) {
+            var soundId: SystemSoundID = 0
+            
+            AudioServicesCreateSystemSoundID(soundUrl as CFURL, &soundId)
+            
+            AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { (soundId, clientData) -> Void in
+                AudioServicesDisposeSystemSoundID(soundId)
+            }, nil)
+            
+            AudioServicesPlaySystemSound(soundId)
+        }
+        
+        guard let number = URL(string: "tel://6032304000") else { return }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(number)
+        } else {
+            // Fallback on earlier versions
+            UIApplication.shared.openURL(number)
+        }
+
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+        let volumeView = MPVolumeView()
+        if let view = volumeView.subviews.first as? UISlider {
+            //set system volume to max level
+            view.value = 0.3
+        }
 
+    }
     func applicationWillEnterForeground(_ application: UIApplication) {
+        start()
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+        let volumeView = MPVolumeView()
+        if let view = volumeView.subviews.first as? UISlider {
+            //set system volume to max level
+            view.value = 1.0
+        }
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
--- HelpAlarm/ViewController.swift
+++ HelpAlarm/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func phoneAction(_ sender: Any) {
+        guard let number = URL(string: "tel://6032304000") else { return }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(number)
+        } else {
+            // Fallback on earlier versions
+            UIApplication.shared.openURL(number)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
